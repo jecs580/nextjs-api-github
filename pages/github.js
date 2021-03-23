57cf00195dc43c9e1ce2bc90a2e916ca05816b52
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-03-22 21:15:36 -0400

    Creacion del diseño de la ruta github

--- pages/github.js
+++ pages/github.js
@@ -1,8 +1,32 @@
 import Layouts from '../components/Layouts'
-const Github = ()=>(
-    <Layouts>
-        <h1>Github</h1>
-    </Layouts>
-)
+const Github = ({user})=>{
+    console.log(user);
+    return(
+        <Layouts>
+            <div className="row">
+                <div className="col-md-4 offset-md-4">
+                    <div className="card card-body text-center">
+                        <h1>{user.name}</h1>
+                        <img src={user.avatar_url} alt=""/>
+                        <p>{user.bio}</p>
+                        <a href={user.blog} target="_blank" className="btn btn-outline-secondary my-2">My Blog</a>
+                        <a href={user.html_url} target="_blank" className="btn btn-outline-secondary">Go to Github</a>
+                    </div>
+                </div>
+            </div>
+        </Layouts>
+    )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch('https://api.github.com/users/jecs580');
+    const data = await res.json();
+    console.log(data);
+    return {
+        props:{
+            user:data
+        }
+    }
+}
 
 export default Github;
